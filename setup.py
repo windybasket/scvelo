e78dd0bce0f76e3ca8d6445b78b07c4f010f5f01
windy basket <20081810+windybasket@users.noreply.github.com>
2019-12-25 11:29:08 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     setup_requires=['setuptools_scm'],
     python_requires='>=3.6',
     install_requires=['scanpy>=1.4', 'anndata>=0.6.21', 'numpy>=1.17', 'pandas>=0.23', 'scikit-learn>=0.21.2',
-                      'scipy>=1.0', 'matplotlib=3.0', 'umap-learn>=0.3', 'loompy>=2.0.12'],
+                      'scipy>=1.0', 'matplotlib==3.0', 'umap-learn>=0.3', 'loompy>=2.0.12'],
     packages=find_packages(),
     author="Volker Bergen",
     author_email="volker.bergen@helmholtz-muenchen.de",
